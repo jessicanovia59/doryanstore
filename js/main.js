191f2da2ac930b18b8eaff396d754db7ace55744
jesnov29 <63089347+jessicanovia59@users.noreply.github.com>
2022-01-14 13:55:46 +0700

    Folder javascript yang digunakan pada web

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".product-layout").slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        infinite: true,
+        lazyLoad: 'ondemand',
+    });
+
+    $(".promo").slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        infinite: true,
+        lazyLoad: 'ondemand',
+    });
+})
